8c0ed721a450ccbccda0bd4e654dba930c0fc96a
Vinicius Calixto <vc.viniciuscalixto@gmail.com>
2022-01-25 21:51:52 -0300

    Iniciando curso
    Adicionando arquivos iniciais aula 01 e 02

---
+++ Aulas/Aula02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula02{
+    class Principal{
+        static void Main(string[] args){
+            
+            Console.WriteLine("Olá Mundo...\n");
+            Console.WriteLine("CBF Cursos\n");
+            
+            //validando Argumentos de entrada.
+            if(args.GetLength(0) > 0){
+                Console.WriteLine(args.GetValue(0));
+            }
+        }
+    }
+}
